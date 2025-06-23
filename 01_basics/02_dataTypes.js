b9dcc2e7cf4b1aad639f868d8059a48372a21ec6
ssr218 <sachinsinghrathore35@gmail.com>
2025-06-23 17:51:06 +0000

    data types done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js cose as newer version
+//alert(3+3) we are using node js not browser
+
+// code readebility is necessary
+
+let name = "sachin"
+let age = 22
+let isLoggedIn = false
+
+//numer => 2 to the power 53/52
+//bigint => 
+// string => ""
+//boolean => true/false
+//null => standalone value  let state = null
+// undefined => value not defined   let state;
+//symbol => unique components
+
+
+// object
+
+
+console.log(typeof "sachin")
+console.log(typeof age)
+console.log(typeof null)      // object
+console.log(typeof undefined) // undifined
