084f434b9575184195aff9bb9d829b02088edd7d
ssr218 <sachinsinghrathore35@gmail.com>
2025-06-24 08:47:35 +0000

    conversion done

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,13 @@
+// let score = "33abc"
+// let score = "33"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "sachin"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+
