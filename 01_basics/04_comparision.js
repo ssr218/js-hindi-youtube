f9a8027b8cec60112982c7b04c5984c226de28e0
ssr218 <sachinsinghrathore35@gmail.com>
2025-06-26 06:44:47 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// strict equality alwyas checks for both data types and the values 
+
+// console.log("2" === 2);
