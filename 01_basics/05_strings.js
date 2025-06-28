61f7c80b2d76acf403c439b47f009c0cb58a01e0
ssr218 <sachinsinghrathore35@gmail.com>
2025-06-28 14:03:40 +0000

    strings done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,46 @@
+const name = "sachin"
+
+const repoCount = 50
+
+// console.log(name + repoCount + " Value")
+
+// console.log('Hello my name is ${name} and my repoCount is ${repoCount}');
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);/
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('h'));
+
+// const newString = gameName.substring(-8,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-2,-4);
+// console.log(anotherString);
+
+const newStringOne = "   hitesh  "
+// console.log(newStringOne);
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+// console.log(url.replace('%20', '-'));
+
+console.log(url.includes('sundar'))
+console.log(gameName.split('-'))
+
+
+
+
+
+
+
+
+
+
+
